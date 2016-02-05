8ee88733a641cec1851f0db4a68677e9faa75b8a
David "Blackmagic" Elahee <david.elahee@gmail.com>
2016-02-05 10:15:39 +0100

    add bpm/bps helpers
    not perfect but I don't know why...I think integer timePerQuarter encoding cannot really be inverted easily

--- com/newgonzo/midi/file/messages/SetTempoMessage.hx
+++ com/newgonzo/midi/file/messages/SetTempoMessage.hx
@@ -5,11 +5,19 @@ class SetTempoMessage extends MetaEventMessage {
 	public var microsecondsPerQuarter(default, null):UInt;
 	public var tempo(default, null):UInt;
 	
+	public var bpm:Float;
+	public var bps:Float;
+	
 	public function new (microsecondsPerQuarter:UInt) {
 		super(MetaEventMessageType.SET_TEMPO);
 		
 		this.microsecondsPerQuarter = microsecondsPerQuarter;
 		this.tempo = Std.int(microsecondsPerQuarter / 24000);
+		
+		//this gives strange fractionnal dusts but haven't found a better calculus...
+		//for a good result, we should round bpm then redivide by 60 for bps
+		bpm = 60000000.0 / microsecondsPerQuarter;
+		bps = (60000000.0/60.0) / microsecondsPerQuarter;
 	}
 	
 	override public function toString () :String {
